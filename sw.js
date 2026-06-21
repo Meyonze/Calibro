409531e7340b0cbd69c46e98fc1f086f41baf745
Meyonze <lieaflet@gmail.com>
2026-06-21 23:06:01 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+// Calibro Service Worker
+// 方針: ネットワーク優先、オフライン時のみキャッシュにフォールバック。
+// 開発中にindex.htmlを頻繁に更新する想定なので、古いキャッシュに固定されないよう
+// あえて「キャッシュ優先」ではなく「ネットワーク優先」にしている。
+
+const CACHE_NAME = 'calibro-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then((cached) => cached || caches.match('./index.html'))
+      )
+  );
+});
